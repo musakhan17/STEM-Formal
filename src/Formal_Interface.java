81e3cbeb16a04a900032ccfe87a8ed734b5fb84f
Musa Khan <mmkhan@knox.edu>
2020-08-07 19:04:28 -0500

    Minor updates

--- src/Formal_Interface.java
+++ src/Formal_Interface.java
@@ -2,6 +2,6 @@ public interface Formal_Interface {
 
     public void add(String person, String food); // Adds a new guest and their dietary preference to the formal event list
     public void remove(String person); // Removes a guest and their dietary preference from the formal event list
-    public void get(); //Prints list of guests and their dietary preference
-    public void change();
+    public void get(); // Gets the dietary preference of a guest
+    public void change(); // Changes the dietary preference of a guest
 }
